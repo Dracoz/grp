4b22adc91b6b707ad6f64c5809ddefc91511af52
Dracoz <dracozlee95@gmail.com>
2024-12-11 10:28:31 +0800

    changed indicator

--- components/Indicator.jsx
+++ components/Indicator.jsx
@@ -4,7 +4,7 @@ export default function Indicator(props){
     return (
         <li>
             <span class="flex items-center text-sm font-medium text-gray-900 dark:text-white me-3">
-                <span class={`flex w-2.5 h-2.5 rounded-full me-1.5 flex-shrink-0 ${props.color}`}></span>{props.children}
+                <span class={`flex w-3.5 h-2.5 rounded-full me-1.5 flex-shrink-0 ${props.color}`}></span>{props.children}
             </span>
         </li>
     );
