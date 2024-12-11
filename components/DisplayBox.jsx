54941296e5c0c87aa177b5d6050d508cc0ed8018
Dracoz <dracozlee95@gmail.com>
2024-12-11 10:21:22 +0800

    updated color

--- components/DisplayBox.jsx
+++ components/DisplayBox.jsx
@@ -2,7 +2,7 @@ export default function DisplayBox({value, name}){
     return (
         <div className="flex flex-col items-center py-10">
             <div className='bg-slate-500 px-2 py-2 w-40 flex justify-center'>
-                <p className="text-5xl">{value}</p>
+                <p className="text-5xl text-red-500">{value}</p>
             </div>
             <p className="text-white">{name}</p>  
         </div>
