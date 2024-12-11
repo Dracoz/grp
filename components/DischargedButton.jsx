0a1604fd08f2c7eadf3db3b571bf840d8a0fa384
Dracoz <dracozlee95@gmail.com>
2024-12-11 11:19:03 +0800

    changed text color

--- components/DischargedButton.jsx
+++ components/DischargedButton.jsx
@@ -6,7 +6,7 @@ import Button from '@/components/Button'
 export default function DischargedButton({...others}){
     return (
         <ol>
-            {DISCHARGE_BUTTON.map(item => <Button className={`${styles.pressButton} bg-sky-500 hover:bg-sky-700`}>{item}</Button>)}       
+            {DISCHARGE_BUTTON.map(item => <Button className={`${styles.pressButton} bg-sky-500 hover:bg-sky-700 text-zinc-200`}>{item}</Button>)}       
         </ol>
     )
 }
